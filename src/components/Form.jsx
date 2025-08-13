447be56fb579c602e8dea27efb9c4c14c1e2a53c
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-13 16:10:25 +0200

    add educataion useState and props

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,11 +5,12 @@ import { useState } from "react";
 
 export default function Form(){
  const [person, setPerson] = useState({name: "", email: "", phone: ""});
+ const [education, setEducation] = useState({school: "", studie: "", date: ""});
 
     return(
         <form>
             <GeneralForm onClick={setPerson} person={person}/>
-            <EducationForm />
+            <EducationForm onClick={setEducation} education={education}/>
             <PracticalForm />
         </form>
     )
