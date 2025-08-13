f5ec52b8865b9bc7954e7ffa1e10aff268f8d09b
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-13 16:24:12 +0200

    add practical form useState, props and onChange listener

--- src/components/PracticalForm.jsx
+++ src/components/PracticalForm.jsx
@@ -1,22 +1,45 @@
-export default function PracticalForm(){
+import { useState } from "react"
+
+export default function PracticalForm({onClick, practical}){
+    const [localPractical, setLocalPractical] = useState(practical);
+
     return(
         <div>
-            <div class="form-example">
-                <label for="company-name">Enter the company name: </label>
-                <input type="text" name="company-name" id="company-name" required />
+            <div className="form-practical-input">
+                <label htmlFor="company">Enter the company name: </label>
+                <input type="text" 
+                name="company" 
+                id="company"
+                value={localPractical.company}
+                onChange={(e) => {setLocalPractical({...localPractical, company: e.target.value})}} />
             </div>
-            <div class="form-example">
-                <label for="position-title">Enter position title: </label>
-                <input type="text" name="position-title" id="position-title" required />
+            <div className="form-practical-input">
+                <label htmlFor="position">Enter position title: </label>
+                <input type="text" 
+                name="position" 
+                id="position"
+                value={localPractical.position}
+                onChange={(e) => {setLocalPractical({...localPractical, position: e.target.value})}} />
             </div>
-            <div class="form-example">
-                <label for="position-first-date">Enter your position first date: </label>
-                <input type="date" name="position-first-date" id="position-first-date" required />
+            <div className="form-practical-input">
+                <label htmlFor="position-first-date">Enter your position first date: </label>
+                <input type="date" 
+                name="position-first-date" 
+                id="position-first-date"
+                value={localPractical.firstDate}
+                onChange={(e) => {setLocalPractical({...localPractical, firstDate: e.target.value})}} />
             </div>
-            <div class="form-example">
-                <label for="position-last-date">Enter your position last date: </label>
-                <input type="date" name="position-last-date" id="position-last-date" required />
+            <div className="form-practical-input">
+                <label htmlFor="last-date">Enter your position last date: </label>
+                <input type="date" 
+                name="last-date" 
+                id="last-date"
+                value={localPractical.lastDate}
+                onChange={(e) => {setLocalPractical({...localPractical, lastDate: e.target.value})}} />
             </div>
+            <button onClick={()=>
+                onClick(...practical, ...localPractical)
+            }>Add</button>
         </div>
     )
 }
