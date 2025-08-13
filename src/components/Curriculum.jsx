ec06657e4ae35f61fd99cdcae87676719efdcbfb
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-13 16:50:08 +0200

    create curriculum component

---
+++ src/components/Curriculum.jsx
@@ -0,0 +1,11 @@
+import CurriculumPerson from "./CurriculumPerson"
+
+export default function Curriculum({person, education, practical}){
+    return(
+    <div className="curriculum">
+    <CurriculumPerson person={person}/>
+    {/* <CurriculumEducation education={education}/>
+    <CurriculumPractical person={practical}/> */}
+    </div>
+    )
+}
