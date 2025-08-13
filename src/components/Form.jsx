117dfc5b34b642411d0f4c921dbfe77fb731767a
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-13 13:05:12 +0200

    add form components import

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,14 @@
 import GeneralForm from "./GeneralForm"
+import EducationForm from "./EducationForm"
+import PracticalForm from "./PracticalForm"
 
 export default function Form(){
 
     return(
         <form action="">
-            
+            <GeneralForm />
+            <EducationForm />
+            <PracticalForm />
         </form>
     )
 }
