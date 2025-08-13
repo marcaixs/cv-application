0f380ba02f54823a1fb5d7e16e88a0010a95bd68
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-13 14:12:44 +0200

    add person useState as prompt and submit button

--- src/components/GeneralForm.jsx
+++ src/components/GeneralForm.jsx
@@ -1,18 +1,40 @@
-export default function GeneralForm(){
+import { useState } from "react"
+
+export default function GeneralForm({onClick, person}){
+     const [localPerson, setLocalPerson] = useState(person);
+
     return(
-        <div>
-            <div class="form-example">
+        <div className="form-general">
+            <div className="form-general-input">
                 <label for="name">Enter your name: </label>
-                <input type="text" name="name" id="name" required />
+                <input 
+                type="text" 
+                name="name" 
+                id="name"
+                value={localPerson.name}
+                onChange={(e)=> setLocalPerson({...localPerson, name: e.target.value})}  />
             </div>
-            <div class="form-example">
+            <div className="form-general-input">
                 <label for="email">Enter your email: </label>
-                <input type="email" name="email" id="email" required />
+                <input 
+                type="email" 
+                name="email" 
+                id="email"
+                value={localPerson.email}
+                onChange={(e)=> setLocalPerson({...localPerson, email: e.target.value})}  />
             </div>
-            <div class="form-example">
+            <div className="form-general-input">
                 <label for="phone">Enter your phone number: </label>
-                <input type="text" name="phone" id="phone" required />
+                <input 
+                type="text" 
+                name="phone" 
+                id="phone"
+                value={localPerson.phone}
+                onChange={(e)=> setLocalPerson({...localPerson, phone: e.target.value})}  />
             </div>
+            <button onClick={() => onClick(
+                {...person, ...localPerson }
+            )}>Submit</button>
         </div>
     )
 }
