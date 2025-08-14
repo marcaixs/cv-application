68505158154072ed14a08f24afe58c08b93b776b
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-14 12:03:28 +0200

    add curriculumPractical component render

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -1,12 +1,13 @@
 import CurriculumPerson from "./CurriculumPerson"
 import CurriculumEducation from "./CurriculumEducation"
+import CurriculumPractical from "./CurriculumPractical"
 
 export default function Curriculum({person, education, practical}){
     return(
     <div className="curriculum">
     <CurriculumPerson person={person}/>
     <CurriculumEducation education={education}/>
-    {/* <CurriculumPractical person={practical}/> */}
+    <CurriculumPractical practical={practical}/>
     </div>
     )
 }
