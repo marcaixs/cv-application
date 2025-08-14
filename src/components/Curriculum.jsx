c25aa86297374584097e51c30afce0c934cbf14c
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-14 12:05:07 +0200

    add titles to curriculum render

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -5,8 +5,12 @@ import CurriculumPractical from "./CurriculumPractical"
 export default function Curriculum({person, education, practical}){
     return(
     <div className="curriculum">
+    <h2>Curriculum Vitae</h2>
+    <h3>Personal Information</h3>    
     <CurriculumPerson person={person}/>
+    <h3>Education</h3>
     <CurriculumEducation education={education}/>
+    <h3>Practical Experience</h3>
     <CurriculumPractical practical={practical}/>
     </div>
     )
