45b46130995edbea750f24bb551de46204d0def9
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-14 11:35:48 +0200

    add curriculumEducation to render

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -1,11 +1,12 @@
 import CurriculumPerson from "./CurriculumPerson"
+import CurriculumEducation from "./CurriculumEducation"
 
 export default function Curriculum({person, education, practical}){
     return(
     <div className="curriculum">
     <CurriculumPerson person={person}/>
-    {/* <CurriculumEducation education={education}/>
-    <CurriculumPractical person={practical}/> */}
+    <CurriculumEducation education={education}/>
+    {/* <CurriculumPractical person={practical}/> */}
     </div>
     )
 }
