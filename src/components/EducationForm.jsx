eefec9ac5b10cee6b79f90c9a3eb4710dd6dceb6
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-16 12:04:57 +0200

    add unique id to each card and delete button

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -1,10 +1,26 @@
 import { useState } from "react"
 
 export default function EducationForm({onClick, education}){
-    const [localEducation, setLocalEducation] = useState({school: "", studie: "", date: ""});
+    const [localEducation, setLocalEducation] = useState({id: "", school: "", studie: "", date: ""});
+
+    const handleDelete = (id) => {
+        const updatedEducation = education.filter((item) => item.id !== id);
+        onClick(updatedEducation);
+    };
+    
+    const cards = education.map(item => 
+    <div className="education-card" key={item.id}>
+        <p>{item.school}</p>
+        <p>{item.studie}</p>
+        <p>{item.date}</p>
+        <button onClick={
+            () => handleDelete(item.id)
+        }>Delete</button>
+    </div>)
 
     return(
         <div className='form-education'>
+            <h3>Introduce your education background</h3>
             <div className="form-education-input">
                 <label htmlFor="school">Enter your school name: </label>
                 <input 
@@ -34,8 +50,13 @@ export default function EducationForm({onClick, education}){
             </div>
              <button onClick={(e) =>{
                 e.preventDefault();
+                setLocalEducation({...localEducation, id: crypto.randomUUID()})
                 onClick([...education, localEducation])
                 }}>Add</button>
+            <h4>Submitted education</h4>
+            <div>
+                {cards}
+            </div>    
         </div>
     )
 }
