d18ad2c76597ab5b83162ea3790fecf309acb5e3
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-22 16:04:59 +0200

    add handleEdit function

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -1,58 +1,38 @@
 import { useState } from "react"
+import InputsEducation from "./InputsEducation";
 
 export default function EducationForm({onClick, education}){
-    const [localEducation, setLocalEducation] = useState({id: "", school: "", studie: "", date: ""});
+    const [localEducation, setLocalEducation] = useState({id: crypto.randomUUID(), school: "", studie: "", date: ""});
 
     const handleDelete = (id) => {
         const updatedEducation = education.filter((item) => item.id !== id);
         onClick(updatedEducation);
     };
+
+    const handleEdit = (id) => {
+        setLocalEducation(education.find((item)=> item.id == id))
+    }
     
     const cards = education.map(item => 
-    <div className="education-card" key={item.id}>
-        <p>{item.school}</p>
-        <p>{item.studie}</p>
-        <p>{item.date}</p>
-        <button onClick={
-            () => handleDelete(item.id)
-        }>Delete</button>
-    </div>)
+        <div className="education-card" key={item.id}>
+            <p>{item.school}</p>
+            <p>{item.studie}</p>
+            <p>{item.date}</p>
+
+            <button onClick={
+                () => handleEdit(item.id)
+            }>Edit</button>
+
+            <button onClick={
+                () => handleDelete(item.id)
+            }>Delete</button>
+
+        </div>
+    )
 
     return(
         <div className='form-education'>
-            <h3>Introduce your education background</h3>
-            <div className="form-education-input">
-                <label htmlFor="school">Enter your school name: </label>
-                <input 
-                type="text" 
-                name="school" 
-                id="school"
-                value={localEducation.school}
-                onChange={(e)=> setLocalEducation({...localEducation, school: e.target.value})} />
-            </div>
-            <div className="form-education-input">
-                <label htmlFor="studie-title">Enter studie title: </label>
-                <input 
-                type="text" 
-                name="studie" 
-                id="studie"
-                value={localEducation.studie}
-                onChange={(e)=> setLocalEducation({...localEducation, studie: e.target.value})} />
-            </div>
-            <div className="form-education-input">
-                <label htmlFor="studie-date">Enter your studies date: </label>
-                <input 
-                type="date" 
-                name="studie-date" 
-                id="date" 
-                value={localEducation.date}
-                onChange={(e)=> setLocalEducation({...localEducation, date: e.target.value})}/>
-            </div>
-             <button onClick={(e) =>{
-                e.preventDefault();
-                setLocalEducation({...localEducation, id: crypto.randomUUID()})
-                onClick([...education, localEducation])
-                }}>Add</button>
+            <InputsEducation localEducation={localEducation} setLocalEducation={setLocalEducation} education={education} onClick={onClick}/>
             <h4>Submitted education</h4>
             <div>
                 {cards}
