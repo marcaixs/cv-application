a5f11932be0d25214c273a84d8ebe93209708749
Marc <147423007+marcaixs@users.noreply.github.com>
2025-08-22 16:08:42 +0200

    generate id on useState

--- src/components/PracticalForm.jsx
+++ src/components/PracticalForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 export default function PracticalForm({onClick, practical}){
-    const [localPractical, setLocalPractical] = useState({id: "", company: "", position:"", firstDate: "", lastDate: ""});
+    const [localPractical, setLocalPractical] = useState({id: crypto.randomUUID(), company: "", position:"", firstDate: "", lastDate: ""});
 
     function handleDelete(id){
         const updatedPractical = practical.filter((item)=> item.id != id);
@@ -21,43 +21,7 @@ export default function PracticalForm({onClick, practical}){
 
     return(
         <div className="form-practical">
-            <div className="form-practical-input">
-                <label htmlFor="company">Enter the company name: </label>
-                <input type="text" 
-                name="company" 
-                id="company"
-                value={localPractical.company}
-                onChange={(e) => {setLocalPractical({...localPractical, company: e.target.value})}} />
-            </div>
-            <div className="form-practical-input">
-                <label htmlFor="position">Enter position title: </label>
-                <input type="text" 
-                name="position" 
-                id="position"
-                value={localPractical.position}
-                onChange={(e) => {setLocalPractical({...localPractical, position: e.target.value})}} />
-            </div>
-            <div className="form-practical-input">
-                <label htmlFor="position-first-date">Enter your position first date: </label>
-                <input type="date" 
-                name="position-first-date" 
-                id="position-first-date"
-                value={localPractical.firstDate}
-                onChange={(e) => {setLocalPractical({...localPractical, firstDate: e.target.value})}} />
-            </div>
-            <div className="form-practical-input">
-                <label htmlFor="last-date">Enter your position last date: </label>
-                <input type="date" 
-                name="last-date" 
-                id="last-date"
-                value={localPractical.lastDate}
-                onChange={(e) => {setLocalPractical({...localPractical, lastDate: e.target.value})}} />
-            </div>
-            <button onClick={(e)=>{
-                e.preventDefault();
-                setLocalPractical({...localPractical, id: crypto.randomUUID()})
-                onClick([...practical, localPractical])
-                }}>Add</button>
+           
                 <h4>Submitted work experience</h4>
             <div>
                 {cards}
